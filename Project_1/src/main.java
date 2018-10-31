40e69523a4fd393b3df37dd64cdfc5d28271b951
thang <thang@DESKTOP-T26I80C>
2018-10-31 16:30:32 +0100

    First Commit of Project 1 Test

---
+++ Project_1/src/main.java
@@ -0,0 +1,21 @@
+
+public abstract class main {
+
+	public main() {
+		// Nguyen Hung Thang (10/09/1991)
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+
+}
